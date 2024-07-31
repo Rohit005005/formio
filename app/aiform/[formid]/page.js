9b83f61047cdb0fb25bdb4bde78b083c89dd3dff
Rohit005005 <137618172+Rohit005005@users.noreply.github.com>
2024-07-31 14:53:23 +0530

    fixed live preview bg issue

--- app/aiform/[formid]/page.js
+++ app/aiform/[formid]/page.js
@@ -26,7 +26,7 @@ function LiveAiForm({ params }) {
   };
   return (
     <div
-      className="p-10 flex justify-center items-center h-screen"
+      className="p-10 flex justify-center items-center h-full"
       style={{ backgroundImage: record?.background ,backgroundSize:"cover"}}
     >
       {record && (
